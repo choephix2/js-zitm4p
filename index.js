b8f943033fcef1208204e993465bd8aeba877293
choephix2 <choephix2@gmail.com>
2020-07-07 14:59:54 +0300

    1 file modified, 3 added, 2 deleted

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-// Import stylesheets
-import './style.css';
+import "./styles.css";
 
-const HTML_Slots = `
-<h1>Slots Game</h1>
-<div>...</div>
-<script src="/slots.js"></script>
-`
+const INDEX_SLOTS = {
+  html: `
+  <div>
+    <h1>Slots Game</h1>
+    <div>...</div>
+  </div>
+  <script src="/slots.js"></script>
+  `,
+  scripts: ["/common.js", "/slots.js"]
+};
 
-const HTML_LiveCasino = `
-<h1>Live Casino Game</h1>
-<div>[ le game ]</div>
-<script src="/live-casino.js"></script>
-`
+const INDEX_LIVE_CASINO = {
+  html: `
+  <div>
+    <h1>Live Casino Game</h1>
+    <div>[ le game ]</div>
+  </div>
+  <script src=></script>
+  `,
+  scripts: ["/common.js", "/live-casino.js"]
+};
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const gameCategory = urlParams.get('cat')
+const gameCategory = urlParams.get("cat");
 
-document.body.innerHTML = 
-  gameCategory === `live-casino`
-  ? HTML_LiveCasino
-  : HTML_Slots;
+const index_contents =
+  gameCategory === `live-casino` ? INDEX_LIVE_CASINO : INDEX_SLOTS;
+
+document.body.innerHTML = index_contents.html;
+for (let src of index_contents.scripts) {
+  let el = document.createElement("script");
+  el.src = `src` + src;
+  document.body.appendChild(el);
+}
---
+++ scripts/slots.js
@@ -0,0 +1,3 @@
+alert(`Henlo slops`);
+
+document.body.innerHTML += `<code>Henlo slops</code>`;
